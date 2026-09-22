bc04a7f30b9e0d172fbe5ec57e174baae79e8815
Ashrin Tamim <ashrin@echologyx.com>
2026-09-23 05:49:03 +0600

    chapter 10 done

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -90,6 +90,7 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+  console.log('fetchFilteredInvoices query:', query);
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -11,6 +11,8 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
+  console.log('InvoicesTable query:', query);
+  
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
